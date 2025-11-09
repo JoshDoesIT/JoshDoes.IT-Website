9c2e13b64237c2edc8fe781c8a91d608a3a9b058
Joshua Jones <devansmain@gmail.com>
2025-11-09 14:46:12 -0500

    Update favicon reference to use favicon.ico only

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,8 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
-        <link rel="alternate icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         <Script
           src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js"
           crossOrigin="anonymous"
